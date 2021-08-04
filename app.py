ed58002624cb5cfa264c1314a0da7f4848a15fd7
Paul BOUAFFOU <43551198+paulbouaffou@users.noreply.github.com>
2021-08-04 23:52:45 +0000

    Modification pour la mise en maintenance

--- app.py
+++ app.py
@@ -11,71 +11,27 @@ import update
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["POST", "GET"])
+@app.route("/")
 def home():
 	# return home page
 	
-	# return year actual
-	year = utils.getYear()
-
-	# Number of article in the database
-	number_article = utils.getNumberArticle()
-
-	# If form was submitted
-	if request.method == "POST":
-		limit = 10
-		if request.form['limit'] != "":
-			limit = int(request.form['limit'])
-		# Getting the articles in database
-		articles = utils.getArticles(limit)
-		return render_template('index.html', title = "Résultats | Gawa Côte d'Ivoire ", articles = articles, number_article = number_article, year = year, limit = limit)
-	# If request is GET
-	else:
-		return render_template('index.html', title = "Accueil | Gawa Côte d'Ivoire ", number_article = number_article, year = year)
+	return render_template('index.html', title = "Accueil | Gawa Côte d'Ivoire ")
 
 @app.route("/result")
 def result():
-
-	# return year actual
-	year = utils.getYear()
-
-	# Number of article in the database
-	number_article = utils.getNumberArticle()
 	
-	# Number of article in the database
-	all_articles = utils.getAllArticles()
-	
-	return render_template('result.html', title = "Résultats de tous les articles à améliorer | Gawa Côte d'Ivoire ", number_article = number_article, all_articles = all_articles, year = year)
+	return render_template('index.html', title = "Accueil | Gawa Côte d'Ivoire ")
 	
 @app.route("/updated")
 def updated():
-	# return page without information. But a bot is operating background this page
 
-	# Turn the bot
-	miseajour = update.setup()
-
-	return render_template('api.html', title = "Résultats API | Gawa Côte d'Ivoire ", miseajour = miseajour)
+	return render_template('index.html', title = "Accueil | Gawa Côte d'Ivoire ")
 
 @app.route("/admin")
 def admin():
-
-	# Number of article in the database
-	number_article = utils.getNumberArticle()
-
-	# Number of article in archive Ivory Cost
-	all_article_archive = update.app()
-
-	# Number of article in archive Ivory Cost which has not problem
-	rest_article_archive = update.difference()
-
-	# Date of the moment
-	date_of_now = utils.getDate()
-
-	# return year actual
-	year = utils.getYear()
 	
-	return render_template('admin.html', title = "Administration | Gawa Côte d'Ivoire ", number_article = number_article, all_article_archive = all_article_archive, rest_article_archive = rest_article_archive, date_of_now = date_of_now, year = year)
-
+	return render_template('index.html', title = "Accueil | Gawa Côte d'Ivoire ")
+	
 # Execute the application
 if __name__ == '__main__':
 	app.run()
